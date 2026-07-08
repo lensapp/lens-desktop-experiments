e43b28ef6859c8cf03c40697f501c129e2bc8a4a
Stevan Koncar <stev.koncar@gmail.com>
2026-07-08 09:47:56 +0200

    chore: Cleanup ai provider azure

--- experiments/ai-provider-for-azure-2/src/test-util-runnable.injectable.ts
+++
@@ -1,8 +0,0 @@
-import { getTestUtilRunnableForOverrides } from "@lensapp/test-utils-for-production";
-import globalOverrides from "./**/*.global-override-for-injectable.(ts|tsx)";
-import defaultOverrides from "./**/*.default-override.(ts|tsx)";
-
-export const testUtilRunnableInjectable = getTestUtilRunnableForOverrides({
-  globalOverrides,
-  defaultOverrides,
-});
